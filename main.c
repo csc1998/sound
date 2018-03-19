87a907466e155a5d207513f5302fe87d3bb918ac
Che1998 <1059131263@qq.co>
2018-03-19 13:40:45 +0000

    second commit

---
+++ main.c
@@ -0,0 +1,15 @@
+// this file contain the mian function of our project
+#include "sound.h"
+#include <stdlib.h>
+
+int main(void){
+ 	while(1){		//loop forever
+	  // run system command "arecord" to record 1 sec of wav
+	  system("arecord -r16000 -c1 -f s16_LE -d1 test.wav");
+          // display WAV header
+	  dispWAVHeader("test.wav");
+	  // display WAV strength as decibel value
+	  break;	// loop runs just once
+	}
+	return 0;
+}
